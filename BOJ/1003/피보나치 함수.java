46dd9349f376f3c8438e7db8241a7f6bf07e39a0
ES_CHOI <greatgelly96@gmail.com>
2021-11-23 21:55:00 +0900

    BOJ-1003 '피보나치 함수' [ 21/11/23 21:55:00 ] by Alub

---
+++ BOJ/1003/피보나치 함수.java
@@ -0,0 +1,30 @@
+/**
+* 메모리: 12596 KB, 시간: 112 ms
+* 풀이 시간: 00:00:26
+* 2021.11.23
+* by Alub
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int dp[] = new int[41];
+    static void find(){
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3; i <= 40; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        find();
+        int T = Integer.parseInt(in.readLine());
+        for(int t = 0; t < T; t++){
+            int n = Integer.parseInt(in.readLine());
+            if(n == 0) System.out.printf("%d %d\n", 1, 0);
+            else System.out.printf("%d %d\n", dp[n-1], dp[n]);
+        }
+    }
+}
