a1ec596094805bcaecd8d7a98cbf69f7569c80bd
ES_CHOI <greatgelly96@gmail.com>
2021-12-01 12:01:59 +0900

    BOJ-11650 '좌표 정렬하기' [ 21/12/01 12:01:58 ] by Alub

---
+++ BOJ/11650/test.cpp
@@ -0,0 +1,37 @@
+/**
+* 메모리: 2768 KB, 시간: 44 ms
+* 풀이 시간: 00:00:01
+* 2021.12.01
+* by Alub
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool compare(const pair<int, int>& a, const pair<int, int>& b){
+    if(a.first == b.first)
+        return a.second < b.second;
+    return a.first < b.first;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector< pair<int, int> > location(n);
+    for(int i=0; i<n; i++)
+        cin >> location[i].first >> location[i].second;
+
+    sort(location.begin(), location.end(), compare);
+
+    for(int i=0; i<n; i++)
+        cout << location[i].first << " "<< location[i].second << "\n";
+
+
+    return 0;
+}
